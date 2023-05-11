bbced93013d6691465a39f8ebc502544525029c7
unknown <harshalbaswant>
2023-05-11 21:20:48 +0530

    toggle issue fixed

--- js/resp.js
+++ js/resp.js
@@ -1,7 +1,7 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-navList = document.querySelector('.nav-list')
-rightNav = document.querySelector('.rightNav')
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navList = document.querySelector('.nav-list');
+rightNav = document.querySelector('.rightNav');
 
 burger.addEventListener('click', () => {
     rightNav.classList.toggle('v-class');
